01e33ffa24e3c6f42ebde14843786dbd01fc4a0e
xijiang <872013164@qq.com>
2017-04-18 15:41:51 +0800

    add

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,21 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   	$(document).ready(function () {
-  		
+			console.log('xiexie');
+				$(".content_3").mCustomScrollbar({
+					scrollInertia:600,
+					autoDraggerLength:false
+				});
+				function onScrollCallback(){
+					$(".callback_demo .callback_demo_output").children("em").delay(1000).fadeOut("slow");
+				}
+				function onTotalScrollBackCallback(){
+					$(".callback_demo .callback_demo_output").children("em").delay(1000).fadeOut("slow");
+				}
+				$(".callback_demo a[rel='scrollto-bottom']").click(function(e){
+					e.preventDefault();
+					$(".content_8").mCustomScrollbar("scrollTo","bottom");
+				});
   	});
   }
 
