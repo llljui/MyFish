8833364757286120866fac8de8af1db0be089131
xijiang <872013164@qq.com>
2017-04-19 17:57:35 +0800

    17.57

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +6,15 @@ import { Http } from '@angular/http';
 })
 export class AppComponent {
 myvaluein=(myvalue)=>{
-
+  let search=[];
+  if (!myvalue) {
+  	console.log(myvalue);
+  	return "请输入关键词";
+  }
+  else if (search.length===0) {
+  	console.log(myvalue);
+  	return "搜不到您要的内容！";
+  }
+  else{return myvalue;}
 }
 }
