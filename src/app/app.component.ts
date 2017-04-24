1558b035275d618a566b1e3622e748b8a3317fa6
xijiang <872013164@qq.com>
2017-04-24 14:09:19 +0800

    2017.4.24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from "rxjs/Rx";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@Injectable()
 export class AppComponent {
+constructor(private _http:Http){
+/*   _http.get('http://localhost:4200/src/API/info.json')
+       .map(res=>res.json())
+       .subscribe((res)=>{
+         console.log(res);
+       },(error)=>{
+         console.log(error);
+       });*/
+};
+
 myvaluein=(myvalue)=>{
   let search=[];
   if (!myvalue) {
