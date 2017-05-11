b2f83050b8f337d66cef240b0e029b5b68747441
xijiang <872013164@qq.com>
2017-05-11 17:56:47 +0800

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component,Injectable,OnInit,DoCheck } from '@angular/core';
 import { Http,URLSearchParams } from '@angular/http';
 import { Observable } from "rxjs/Rx";
 import 'rxjs/add/operator/map';
+
 /*import 'rxjs/add/operator/toPromise';*/
 declare var $:any; 
 @Component({
@@ -11,21 +12,32 @@ declare var $:any;
 })
 @Injectable()
 export class AppComponent {
- constructor(public http:Http){}
-public sobj={
-	gets:(value:any) =>{
-		let http:Http;
-		if (!value) {return "请输入关键词";}
-        else{    
-                 value=http.get('./src/API/info.json')
-		                         .map(res=>res.json())
-		                         .subscribe((res)=>{console.log(res.bigjiang1.age)});	                         
-            }
-	}
-}
+public reslog:string;  
+public searv:any=[];  
+constructor(public http:Http){}
+ public search={
+   show:(value)=>{
+     if (!value) {
+       this.reslog='请您输入正确的关键词';
+       this.searv=[];  
+     }
+     else{
+         this.http.get('../assets/info.json')
+                  .map(res=>res.json())
+                  .subscribe((res)=>{ 
+                  this.reslog="搜索结果如下"; 
+                  (()=>{this.searv.push(res.bigjiang1);})();
+                  console.log(this.searv);      
+                },(error)=>{
+                  alert(error);
+                  console.log(error);
+                });
+     } 
+   }
+ }
  ngOnInit(){
  	if(screen.width>1440) {
  		$('.xy_input').css('margin-top','32%');
  	}
-}             
+}      
 }
